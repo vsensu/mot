1b832a9974d99b70e959c65b5f7f81f4adc9a72c
vsensu <vsensu@foxmail.com>
2020-09-16 01:44:47 +0800

    Hello Triangle

--- Examples/GetStarted/HelloTriangle/HelloTriangle.cpp
+++ Examples/GetStarted/HelloTriangle/HelloTriangle.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <string>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -10,9 +11,29 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+const std::string g_vs_code = R"(
+#version 330 core
+layout (location = 0) in vec3 aPos;
+
+void main()
+{
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+}
+)";
+
+const std::string g_fs_code = R"(
+#version 330 core
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+}
+)";
+
 int main()
 {
-    // glfw: initialize and configure
+    // glfw: 初始化glfw并设置openGL版本，设置只使用Core
     // ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -23,9 +44,9 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    // glfw window creation
+    // glfw 创建窗口
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Hello Triangle", nullptr, nullptr);
     if (window == nullptr)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -43,33 +64,93 @@ int main()
         return -1;
     }
 
+    // 顶点数据
     float vertices[] = {
             -0.5f, -0.5f, 0.f,
             0.5f, -0.5f, 0.f,
             0.f, 0.5f, 0.f
     };
-
+    // 顶点缓冲对象
     GLuint VBO;
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // render loop
+    // 顶点数组对象
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    constexpr GLuint locationIndex = 0;
+    constexpr GLint vertexCount = 3;
+    glVertexAttribPointer(locationIndex, vertexCount, GL_FLOAT, GL_FALSE, vertexCount * sizeof(float), nullptr);
+    glEnableVertexAttribArray(locationIndex);
+
+    // 顶点着色器
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    const GLchar *vsCode = g_vs_code.c_str();
+    glShaderSource(vertexShader, 1, &vsCode, nullptr);
+    glCompileShader(vertexShader);
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // 片段着色器
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    const GLchar *fsCode = g_fs_code.c_str();
+    glShaderSource(fragmentShader, 1, &fsCode, nullptr);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Shader program
+    auto shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::LINK_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // clear时填充色
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+    // 主循环
     // -----------
     while (!glfwWindowShouldClose(window))
     {
-        // input
+        // 输入处理
         // -----
         processInput(window);
 
-        // render
-        // ------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        // 渲染
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // 当我们渲染一个物体时要使用着色器程序
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        // 绘制物体
+        glDrawArrays(GL_TRIANGLES, 0, vertexCount);
+
+        // glfw: 交换双缓冲
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
+        // 事件
         glfwPollEvents();
     }
 
